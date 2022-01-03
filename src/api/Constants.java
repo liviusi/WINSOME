0f69e4b05459a96432fd1e0963aa8e7229831722
Giacomo Trapani <giacomo.trapani01@libero.it>
2022-01-03 05:37:57 +0100

    Aggiornato.

--- src/api/Constants.java
+++ src/api/Constants.java
@@ -2,10 +2,19 @@ package api;
 
 public class Constants
 {
-	public final static int BUFFERSIZE = 512;
+	public final static int BUFFERSIZE = 2048;
+
 	public final static String LOGIN_SUCCESS = " has now logged in.";
+	public final static String USER_NOT_REGISTERED = " has yet to sign up.";
+	public final static String CLIENT_ALREADY_LOGGED_IN = "Client has already logged in.";
+	public final static String LOGOUT_SUCCESS = "Client has now logged out.";
+	public final static String LOGOUT_FAILURE = "Client has yet to login.";
 	public final static String DELIMITER = ":";
+
 	public final static String QUIT_STRING = ":q!";
+	public final static String HELP_STRING = "help";
+	public final static String REGISTER_STRING = "register";
+	public final static String LOGIN_STRING = "login";
 
 	private Constants() { }
 }
