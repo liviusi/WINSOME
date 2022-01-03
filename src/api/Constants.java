53ac0f853c319f136e06859ada7efefcb8363662
Giacomo Trapani <giacomo.trapani01@libero.it>
2022-01-03 16:15:02 +0100

    Uniformato.

--- src/api/Constants.java
+++ src/api/Constants.java
@@ -7,7 +7,7 @@ public class Constants
 	public final static String LOGIN_SUCCESS = " has now logged in.";
 	public final static String USER_NOT_REGISTERED = " has yet to sign up.";
 	public final static String CLIENT_ALREADY_LOGGED_IN = "Client has already logged in.";
-	public final static String LOGOUT_SUCCESS = "Client has now logged out.";
+	public final static String LOGOUT_SUCCESS = " has now logged out.";
 	public final static String LOGOUT_FAILURE = "Client has yet to login.";
 	public final static String DELIMITER = ":";
 
@@ -15,6 +15,7 @@ public class Constants
 	public final static String HELP_STRING = "help";
 	public final static String REGISTER_STRING = "register";
 	public final static String LOGIN_STRING = "login";
+	public final static String LOGOUT_STRING = "logout";
 
 	private Constants() { }
 }
